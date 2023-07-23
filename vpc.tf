af84f97636cd940e94fbbc285d2ed7dc897e73ea
Chris Lea <chris.lea@gmail.com>
2023-07-23 13:30:33 -0700

    Fix main CIDR block.

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block           = "10.253.0.0/16"
+  cidr_block           = "10.213.0.0/16"
   enable_dns_hostnames = true
   tags = {
     Name = "Terraform Testing VPC"
