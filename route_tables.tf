1f3b1092a19f1bdedb4316a799a520b404e1b1b3
Chris Lea <chris.lea@gmail.com>
2023-12-15 11:52:10 -0700

    Add NAT Gateway option

--- route_tables.tf
+++ route_tables.tf
@@ -16,3 +16,23 @@ resource "aws_route_table_association" "public_subnet_association" {
   subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
   route_table_id = aws_route_table.main.id
 }
+
+resource "aws_route_table" "nat_gw" {
+  count  = var.create_nat_gateway == true ? 1 : 0
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw[0].id
+  }
+
+  tags = {
+    Name = "Terraform Example NAT Gateway Routing Table"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  count          = var.create_nat_gateway == true ? length(var.private_subnet_cidrs) : 0
+  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+  route_table_id = aws_route_table.nat_gw[0].id
+}
